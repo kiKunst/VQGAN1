e58cbf51a9561b1c2769a076d413373216b33589
kiKunst <99352636+kiKunst@users.noreply.github.com>
2022-02-16 10:42:29 +0100

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,8 @@
 function returnText(){
   element = document.getElementById('textInput').value;
   if (element != null) {
-    if (element.returnText.trim().lenght >= 1)
     input = element;
     console.log(input);
     window.location.href = "/convert?text=" + input;
-  } else {
-    input = "postcard";
-    console.log(input);
-    window.location.href = "convert?text=" + input;
-  }
+  } 
 }
